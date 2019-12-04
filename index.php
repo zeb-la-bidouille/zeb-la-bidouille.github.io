8138e02492cb256d987b76d0f217af94094b07cc
zeb la bidouille <zeb.la.bidouille@gmx.fr>
2019-12-04 22:30:22 +0100

    html => php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>première page</title>
+  </head>
+
+  <body>
+    <?php 
+      echo "php fonctionne :)";
+    ?>
+    it works !
+    <br/>
+    bravo zeb
+  </body>
+</html>
